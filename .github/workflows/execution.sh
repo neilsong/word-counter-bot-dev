1a79fd410d6d151ec46e9adf7b0964bc56ea70fd
Honyant <anthonyindeepspace@gmail.com>
2021-01-20 15:53:59 -0600

    NEIL U DIDNT CHANGE THE AZURE FOLDER TO counter-dev AND IT WASNT DELPOYING

--- .github/workflows/execution.sh
+++ .github/workflows/execution.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-source /home/azureuser/word-counter-bot/venv/bin/activate
-python3 /home/azureuser/word-counter-bot/main.py
+source /home/azureuser/word-counter-bot-dev/venv/bin/activate
+python3 /home/azureuser/word-counter-bot-dev/main.py

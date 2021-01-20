3c403b83ef19efe083fef4ebb2a57eea1f860773
Honyant <anthonyindeepspace@gmail.com>
2021-01-20 16:06:21 -0600

    bruh idek

--- .github/workflows/execution.sh
+++ .github/workflows/execution.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
+mkdir /home/azureuser
+cd /home/azureuser/word-counter-bot-dev/
+git clone https://github.com/neilsong/word-counter-bot-dev.git
 source /home/azureuser/word-counter-bot-dev/venv/bin/activate
 python3 /home/azureuser/word-counter-bot-dev/main.py

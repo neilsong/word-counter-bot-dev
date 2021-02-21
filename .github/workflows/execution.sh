14b036cbfa151043daaa354ca9cbf9a211cb6acb
Honyant <anthonyindeepspace@gmail.com>
2021-02-21 04:12:06 -0600

    wtf pip is so weird, only works when i make the script do it

--- .github/workflows/execution.sh
+++ .github/workflows/execution.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 source /home/azureuser/word-counter-bot-dev/venv/bin/activate
-pip3 install -r requirements.txt
+#pip3 install -r requirements.txt
 python3 /home/azureuser/word-counter-bot-dev/main.py

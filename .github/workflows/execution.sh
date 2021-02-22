275d26620299e243fa22e2513a248a5e930f17b5
Neil Song <song.neil.song@gmail.com>
2021-02-22 15:21:40 -0600

    clean up gitignore, change venv to .venv

--- .github/workflows/execution.sh
+++ .github/workflows/execution.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-source /home/azureuser/word-counter-bot-dev/venv/bin/activate
+source /home/azureuser/word-counter-bot-dev/.venv/bin/activate
 pip3 install -r /home/azureuser/word-counter-bot-dev/requirements.txt
 python3 /home/azureuser/word-counter-bot-dev/main.py

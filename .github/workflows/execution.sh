77c9fd537357572ac306935d31c2efe11484c6a2
Neil Song <song.neil.song@gmail.com>
2021-02-25 09:53:00 -0600

    use poetry in exeuction

--- .github/workflows/execution.sh
+++ .github/workflows/execution.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
-source /home/azureuser/word-counter-bot-dev/.venv/bin/activate
-pip3 install -r /home/azureuser/word-counter-bot-dev/requirements.txt
-python3 /home/azureuser/word-counter-bot-dev/main.py
+cd /home/azureuser/word-counter-bot-dev/
+source .venv/bin/activate
+poetry install
+poetry run python3 main.py

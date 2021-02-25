bcb5602b5edc2df4a8f4a0a5f4f6b9032eadf09d
Neil Song <song.neil.song@gmail.com>
2021-02-25 10:28:35 -0600

    fix $PATH for sudo

--- .github/workflows/execution.sh
+++ .github/workflows/execution.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 cd /home/azureuser/word-counter-bot-dev/
 source .venv/bin/activate
-poetry install --no-dev
-poetry run python3 main.py
+/root/.poetry/bin/poetry install --no-dev
+/root/.poetry/bin/poetry run python3 main.py

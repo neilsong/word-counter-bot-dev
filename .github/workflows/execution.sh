e0b18b23a2fe803b20a0e482c9db6b22ad854567
Neil Song <song.neil.song@gmail.com>
2021-03-12 09:58:26 -0600

    graceful quit with signals for CI/CD

--- .github/workflows/execution.sh
+++ .github/workflows/execution.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 cd /home/azureuser/word-counter-bot-dev/
-source .venv/bin/activate
+
+pid=$( cat pid )
+
+kill -INT $pid
+
+while ! [ -z $( ps -ef | grep "$pid" | grep -v grep | awk '{print $2}' ) ]
+do sleep 0.2
+done
+
 /root/.poetry/bin/poetry install --no-dev
 /root/.poetry/bin/poetry run python3 main.py

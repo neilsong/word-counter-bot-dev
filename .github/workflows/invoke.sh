73b9d7140e4984a7832c557d53908ac6b0a344e7
Neil Song <song.neil.song@gmail.com>
2021-04-02 20:30:00 +0000

    update git diff repo

--- .github/workflows/invoke.sh
+++ .github/workflows/invoke.sh
@@ -7,7 +7,7 @@ then
     curl -X POST -H "Content-Type: application/json" "$1"
 fi
 
-ci_output=$( git diff --name-only --diff-filter=ADMR HEAD^ "./AI/cloud-inference" | grep -v *.md )
+ci_output=$( git diff --name-only --diff-filter=ADMR HEAD^ "./AI/cluster-inference" | grep -v *.md )
 if ! [ -z "$ci_output" ]
 then
     curl -X POST -H "Content-Type: application/json" "$2"

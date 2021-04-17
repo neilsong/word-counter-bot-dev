13b723506aa95ef978969c09180f52393547681b
Neil Song <song.neil.song@gmail.com>
2021-04-16 22:38:14 -0500

    fix !top

--- utilities.py
+++ utilities.py
@@ -335,8 +335,8 @@ async def leaderboard(self, ctx, word, isGlobal):
                 break
     else:
         async for user in ctx.guild.fetch_members(limit=None):
-            if user.bot or str(user.id) not in self.bot.userWords: continue
-            val = mentiontop(self.bot.userWords[str(user.id)], word)
+            if user.bot or user.id not in self.bot.userWords: continue
+            val = mentiontop(self.bot.userWords[user.id], word)
             if val != "None":
                 leaderboard.update({user: val})
             else:
